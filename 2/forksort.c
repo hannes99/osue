d3851f35fa70dd1e503c7f4cebe8eb2fe533d016
Hannes Laimer <e11808227@student.tuwien.ac.at>
2019-12-21 14:36:09 +0100

    removed DEBUG logging and added docs

--- 2/forksort.c
+++ 2/forksort.c
@@ -1,11 +1,14 @@
-//
-// Created by hannesl on 14.12.19.
-//
+/**
+ * @file forksort.c
+ * @author Hannes Laimer <e11808227@student.tuwien.ac.at>
+ * @date 20.12.2019
+ *
+ * @brief File contains an implementation of forksort.
+ *
+ * This is a implementation of forksort, using fork() and pipes
+ */
 
 #include "logger.h"
-
-#define PROGRAM_NAME "forksort"
-#define DEBUG 1
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,9 +16,12 @@
 #include <sys/wait.h>
 #include <string.h>
 
-static FILE *logout;
-
-
+/**
+ * @brief Frees all the memory allocated by a list of strings
+ *
+ * @param list the list of strings
+ * @param c length of the list
+ */
 void freeList(char **list, int c) {
     int i;
     for (i = c; i <= 0; i--) {
@@ -23,119 +29,120 @@ void freeList(char **list, int c) {
     }
 }
 
+/**
+ * @brief Closes both ends of a pipe
+ *
+ * @param pipe array[2] containing the two file-descriptors
+ * @param c length of the list
+ */
 void closeBoth(int pipe[]) {
     if (close(pipe[0])) {
         log_perror("Could not close 0 of pipe");
-        if (DEBUG) { fclose(logout); }
         exit(EXIT_FAILURE);
     }
     if (close(pipe[1])) {
-        log_perror("Could not close 0 of pipe");
-        if (DEBUG) { fclose(logout); }
+        log_perror("Could not close 1 of pipe");
         exit(EXIT_FAILURE);
     }
 }
 
-char **merge(char **a, char **b, int aLen, int len) {
+/**
+ * @brief Merges two arrays of strings into one
+ *
+ * It is assumed that the to arrays passed are already sorted, if they are the returned array will be sorted too.
+ *
+ * @param a the first array of strings
+ * @param b the second array of strings
+ * @param aLen the length of the first array
+ * @param bLen the length of the second array
+ */
+char **merge(char **a, char **b, int aLen, int bLen) {
+    int len = aLen + bLen;
     char **ret = malloc(len*sizeof(char*));
-    if(ret==NULL) exit(EXIT_FAILURE);
+    if(ret==NULL)  {
+        log_perror("Could not malloc memory for merged array");
+        exit(EXIT_FAILURE);
+    }
     int aIndex = 0;
     int bIndex = 0;
-    int bLen = len - aLen;
     int i;
-    if(DEBUG) {
-        fprintf(logout, "MERGE:\n");
-    }
     for (i = 0; i < len; i++) {
         if (aIndex < aLen && (bIndex >= bLen || strcmp(a[aIndex], b[bIndex]) < 0)) {
             ret[i] = a[aIndex++];
-            if(DEBUG) {
-                fprintf(logout, "\t\t %s from 1\n", ret[i]);
-                fflush(logout);
-            }
         } else {
             ret[i] = b[bIndex++];
-            if(DEBUG) {
-                fprintf(logout, "\t\t %s from 2\n", ret[i]);
-                fflush(logout);
-            }
         }
     }
     return ret;
 }
 
+/**
+ * @brief Reads strings separated by newline into a array. The stream from where the strings
+ *        should be red has to be passed as an argument.
+ *
+ * Memory is allocated dynamically and accordingly to the amount needed.
+ *
+ * @param l pointer to where to save the amount of strings red
+ * @param in the stream from where to read from
+ * @return an array of string in the same order they where red
+ */
 char **readWords(int *len, FILE *in) {
     char **list = NULL;
     int currentWordLen;
     int wordCount = 0;
     int c;
 
-    if (DEBUG) {
-        fprintf(logout, "READING...\n");
-        fflush(logout);
-    }
-
     while (feof(in) == 0) {
-        if (DEBUG) {
-            fprintf(logout, "\t\tX");
-            fflush(logout);
-        }
         c = fgetc(in);
         currentWordLen = 0;
         list = realloc(list, (wordCount+1)* sizeof(char*));
+        if(list == NULL) {
+            log_perror("Could not realloc list for words");
+            exit(EXIT_FAILURE);
+        }
         list[wordCount] = malloc(1);
         while (feof(in) == 0 && c != '\n') {
             list[wordCount] = realloc(list[wordCount], sizeof(char)*(currentWordLen+1));
+            if(list[wordCount] == NULL) {
+                log_perror("Could not realloc memory for word");
+                exit(EXIT_FAILURE);
+            }
             list[wordCount][currentWordLen] = (char) c;
             currentWordLen++;
-            if (DEBUG) {
-                fprintf(logout, "%c", list[wordCount][currentWordLen - 1]);
-                fflush(logout);
-            }
             c = fgetc(in);
-            if (DEBUG) {
-                fprintf(logout, ".");
-                fflush(logout);
-            }
         }
         if (currentWordLen > 0) {
             list[wordCount] = realloc(list[wordCount], (currentWordLen+1) * sizeof(char));
+            if(list[wordCount] == NULL) {
+                log_perror("Could not realloc memory for null-termination");
+                exit(EXIT_FAILURE);
+            }
             list[wordCount][currentWordLen] = '\0';
             wordCount++;
         }
-        if (DEBUG) { fprintf(logout, "\n"); }
     }
     *len = wordCount;
     return list;
 }
 
 int main(int argc, char *argv[]) {
-    int out_b = dup(1);
+    int out_b = dup(STDOUT_FILENO);
     init_logger(argv[0], getpid());
-    if (DEBUG == 1) {
-        char logPath[50];
-        sprintf(logPath, "logs/%d.txt", getpid());
-        logout = fopen(logPath, "w");
-    }
     FILE *in = stdin;
     if(argc>1) {
         in = fopen(argv[1], "r");
     }
     int wordCount = 0;
     char **words = readWords(&wordCount, in);
-    if(DEBUG) {fprintf(logout, "CALLED!%d", wordCount);}
     if (wordCount == 1) {
-        fprintf(logout, " DONE(%s)!\n", words[0]);
         write(out_b, words[0], strlen(words[0]));
         write(out_b, "\0", 1);
-        if (DEBUG) { fclose(logout); }
+        freeList(words, wordCount);
         exit(EXIT_SUCCESS);
     } else if (wordCount == 0) {
         log_error("Called with now words!");
-        if (DEBUG) { fclose(logout); }
         exit(EXIT_FAILURE);
     }
-    fprintf(logout, " NOT DONE!\n");
 
     int p_c1In[2];
     int p_c1Out[2];
@@ -144,22 +151,22 @@ int main(int argc, char *argv[]) {
 
     if (pipe(p_c1In) == -1) {
         log_perror("Pipe for c1In failed");
-        if (DEBUG) { fclose(logout); }
+        freeList(words, wordCount);
         exit(EXIT_FAILURE);
     }
     if (pipe(p_c1Out) == -1) {
         log_perror("Pipe for c1Out failed");
-        if (DEBUG) { fclose(logout); }
+        freeList(words, wordCount);
         exit(EXIT_FAILURE);
     }
     if (pipe(p_c2In) == -1) {
         log_perror("Pipe for c2In failed");
-        if (DEBUG) { fclose(logout); }
+        freeList(words, wordCount);
         exit(EXIT_FAILURE);
     }
     if (pipe(p_c2Out) == -1) {
         log_perror("Pipe for c2Out failed");
-        if (DEBUG) { fclose(logout); }
+        freeList(words, wordCount);
         exit(EXIT_FAILURE);
     }
 
@@ -173,10 +180,9 @@ int main(int argc, char *argv[]) {
 
         dup2(p_c1In[0], STDIN_FILENO);
         dup2(p_c1Out[1], STDOUT_FILENO);
-        //close(p_c1Out[1]);
         execlp(argv[0], argv[0], NULL);
-        log_perror("Could not exec in Child 2");
-        if (DEBUG) { fclose(logout); }
+        log_perror("Could not exec in Child 1");
+        freeList(words, wordCount);
         exit(EXIT_FAILURE);
     } else if (pidC1 > 0) {
         int pidC2 = fork();
@@ -189,17 +195,11 @@ int main(int argc, char *argv[]) {
 
             dup2(p_c2In[0], STDIN_FILENO);
             dup2(p_c2Out[1], STDOUT_FILENO);
-            //close(p_c2Out[1]);
             execlp(argv[0], argv[0], NULL);
             log_perror("Could not exec in Child 2");
-            if (DEBUG) { fclose(logout); }
+            freeList(words, wordCount);
             exit(EXIT_FAILURE);
         } else if (pidC2 > 0) { // Parent
-            if (DEBUG) {
-                fprintf(logout, "STARTED %d as child 1\n", pidC1);
-                fprintf(logout, "STARTED %d as child 2\n", pidC2);
-                fflush(logout);
-            }
             if(close(p_c1Out[1]) == -1) {
                 perror("Could not close 1 of c1Out pipe");
             }
@@ -213,39 +213,27 @@ int main(int argc, char *argv[]) {
             if (c1_stdin == NULL) {
                 log_perror("Could not fdopen p_c1In[1]");
                 freeList(words, wordCount);
-                if (DEBUG) { fclose(logout); }
                 exit(EXIT_FAILURE);
             }
             if (c2_stdin == NULL) {
                 log_perror("Could not fdopen p_c2In[1]");
                 freeList(words, wordCount);
-                if (DEBUG) { fclose(logout); }
                 exit(EXIT_FAILURE);
             }
             for (i = 0; i < wordCount; i++) {
                 if (i < nHalf) {
                     int written = fprintf(c1_stdin, "%s\n", words[i]);
-                    if (DEBUG) {
-                        fprintf(logout, "\t %s to c1(%d)\n", words[i], written);
-                        fflush(logout);
-                    }
                     if (written < 1) {
                         log_error("Coudl not write anything to child 1!");
                         freeList(words, wordCount);
-                        if (DEBUG) { fclose(logout); }
                         exit(EXIT_FAILURE);
                     }
                     fflush(c1_stdin);
                 } else {
                     int written = fprintf(c2_stdin, "%s", words[i]);
-                    if (DEBUG) {
-                        fprintf(logout, "\t %s to c2(%d)\n", words[i], written);
-                        fflush(logout);
-                    }
                     if (written < 1) {
                         log_error("Coudl not write anything to child 2!");
                         freeList(words, wordCount);
-                        if (DEBUG) { fclose(logout); }
                         exit(EXIT_FAILURE);
                     }
                     if(i<wordCount-1) {
@@ -257,13 +245,11 @@ int main(int argc, char *argv[]) {
             if (fclose(c1_stdin) != 0) {
                 log_perror("Could not close stdin for child 1");
                 freeList(words, wordCount);
-                if (DEBUG) { fclose(logout); }
                 exit(EXIT_FAILURE);
             }
             if (fclose(c2_stdin) != 0) {
                 log_perror("Could not close stdin for child 2");
                 freeList(words, wordCount);
-                if (DEBUG) { fclose(logout); }
                 exit(EXIT_FAILURE);
             }
 
@@ -271,60 +257,33 @@ int main(int argc, char *argv[]) {
             int secondChildStatus;
 
             waitpid(pidC1, &secondChildStatus, WUNTRACED);
-            if (DEBUG) {
-                fprintf(logout, "C1 terminated\n");
-                fflush(logout);
-            }
             waitpid(pidC2, &firstChildStatus, WUNTRACED);
-            if (DEBUG) {
-                fprintf(logout, "C2 terminated\n");
-                fflush(logout);
-            }
 
             if (WEXITSTATUS(firstChildStatus) == EXIT_SUCCESS && WEXITSTATUS(secondChildStatus) == EXIT_SUCCESS) {
                 freeList(words, wordCount);
                 FILE *c2_stdout = fdopen(p_c2Out[0], "r");
                 FILE *c1_stdout = fdopen(p_c1Out[0], "r");
                 if (c1_stdout == NULL) {
-                    log_perror("Could not fdopen p_c1Out[0]");
+                    log_perror("Could not fdopen stdout of c2");
                     freeList(words, wordCount);
-                    if (DEBUG) { fclose(logout); }
                     exit(EXIT_FAILURE);
                 }
 
                 if (c2_stdout == NULL) {
-                    log_perror("Could not fdopen p_c2Out[0]");
+                    log_perror("Could not fdopen stdout of c2");
                     freeList(words, wordCount);
-                    if (DEBUG) { fclose(logout); }
                     exit(EXIT_FAILURE);
                 }
 
                 int child1Len;
                 char **resultChild1 = readWords(&child1Len, c1_stdout);
-                if (DEBUG) {
-                    fprintf(logout, "got %d words from c1 back\n", child1Len);
-                    fflush(logout);
-                }
 
                 int child2Len;
                 char **resultChild2 = readWords(&child2Len, c2_stdout);
-                if (DEBUG) {
-                    fprintf(logout, "got %d words from c2 back\n", child2Len);
-                    fflush(logout);
-                }
+
+                char **merged = merge(resultChild1, resultChild2, child1Len, child2Len);
 
                 int totalLen = child1Len + child2Len;
-                char **merged = merge(resultChild1, resultChild2, child1Len, totalLen);
-
-                if(DEBUG) {
-                    fprintf(logout, "OUTPUT:\n");
-                    fflush(logout);
-                    for (i = 0; i < totalLen; i++) {
-                        fprintf(logout, "%s\n", merged[i]);
-                        fflush(logout);
-                    }
-                }
-
                 for (i = 0; i < totalLen; i++) {
                     if(write(out_b, merged[i], strlen(merged[i])) != strlen(merged[i])){
                         log_error("Didnt write word successfully!");
@@ -335,33 +294,22 @@ int main(int argc, char *argv[]) {
                         }
                     }
                 }
-
-                if (DEBUG) { fclose(logout); }
                 freeList(words, wordCount);
                 exit(EXIT_SUCCESS);
             } else {
-                if (DEBUG) {
-                    fprintf(logout, "A child terminated with error!\n");
-                    fflush(logout);
-                }
                 log_error("One child didn't exit properly!");
-                if (DEBUG) { fclose(logout); }
-
+                freeList(words, wordCount);
                 exit(EXIT_FAILURE);
             }
         } else {
             log_error("Child 2 could not be forked!");
-            if (DEBUG) { fclose(logout); }
+            freeList(words, wordCount);
             exit(EXIT_FAILURE);
         }
     } else {
         log_error("Child 1 could not be forked!");
-        if (DEBUG) { fclose(logout); }
-
+        freeList(words, wordCount);
         exit(EXIT_FAILURE);
     }
-    if (DEBUG) { fclose(logout); }
-    exit(EXIT_SUCCESS);
-
 }
 
--- 2/logger.c
+++ 2/logger.c
@@ -1,20 +1,45 @@
-//
-// Created by hannesl on 14.12.19.
-//
+/**
+ * @file logger.c
+ * @author Hannes Laimer <e11808227@student.tuwien.ac.at>
+ * @date 20.12.2019
+ *
+ * @brief File contains an implementation of a logger.
+ *
+ * This is an implementation of a logger which prints error-messages to stderr
+ */
 
 #include <stdio.h>
 
 static char *p_name;
 static int p_pid;
 
+/**
+ * @brief Initializes the logger
+ *
+ * @param p name of the program
+ * @param pid process-id of the program
+ */
 void init_logger(char *p, int pid){
     p_name = p;
     p_pid = pid;
 }
 
+/**
+ * @brief writes a message to stderr
+ *
+ * Program name and PID are added in front of the message itself
+ *
+ * @param error the message
+ */
 void log_error(char *error) {
     fprintf(stderr, "%s(%d): %s\n", p_name, p_pid, error);
 }
+
+/**
+ * @brief Calls perror() with program name and PID
+ *
+ * @param desc description of where the error occured
+ */
 void log_perror(char *desc) {
     fprintf(stderr, "%s(%d): ", p_name, p_pid);
     perror(desc);
